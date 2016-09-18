f9613808157b27ef18f392adf23c78d44d491105
Tse Kit Yam <me@kytse.com>
2016-09-18 20:57:36 +0800

    add one more cat to the page
    Javascript is being used to add cats to the page, no matter how many
    cats needs to be added, the addition can be done in JavaScript easily

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
-var cat = document.getElementById('cat-image');
-var result = document.getElementById('number-of-clicks');
-cat.addEventListener('click', function(){
-  var value = parseInt(result.textContent);
-  result.textContent = value + 1;
-}, false);
+var HTMLcat = '<p>%name%</p><p>Number of clicks:</p><p id="clicks-%name%" class="clicks">0</p></div><img class="cat-image" src="img/cat_%name%.jpg" alt="lovely kitten %name% is looking at you">'
+
+
+var cats = document.getElementById('cats');
+var cat;
+
+cat = document.createElement('div');
+cat.innerHTML = HTMLcat.replace(/%name%/g, 'Alice');
+cats.appendChild(cat);
+
+cat = document.createElement('div');
+cat.innerHTML = HTMLcat.replace(/%name%/g, 'Bob');
+cats.appendChild(cat);
+
+var catImages = document.getElementsByClassName('cat-image');
+for (catImage of catImages) {
+  catImage.addEventListener('click', function(){
+    var name = this.parentNode.firstChild.textContent;
+    var clicks = document.getElementById('clicks-%name%'.replace('%name%', name));
+    var value = parseInt(clicks.textContent);
+    clicks.textContent = value + 1;
+  }, false);
+}
